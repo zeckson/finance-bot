63295bf608c6c3d80bc122e6f63a6d03516f8c76
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2024-12-21 15:24:35 +0530

    add sent message

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
-import { printChangeRate } from "./src/finance.text.ts"
+import { printChangeRate, printSentUSDT } from "./src/finance.text.ts"
 
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
   console.log(printChangeRate(42_499.35, 404.915734));
+  console.log(printSentUSDT(404, 1));
 }
