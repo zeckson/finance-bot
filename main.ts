799fc9d2d6d943744fc6b30885ac48bfcaed1f96
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2024-12-22 17:03:09 +0530

    update finance

--- main.ts
+++ main.ts
@@ -7,9 +7,10 @@ import { printChangeRate, printSentUSDT } from "./src/finance.text.ts"
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
   console.log(printChangeRate(42_499.35, 404.915734));
-  console.log(Markdown.exchange(9600, 92.843326));
+  console.log(Markdown.roubles2usdt(9600, 92.843326));
   console.log(printSentUSDT(404, 1));
   console.log(Markdown.sent(92, 1));
+  console.log(Markdown.usdt2lkr(543.34, 165_000));
 
   try {
     await bot.launch( () => {
--- src/finance.md.ts
+++ src/finance.md.ts
@@ -2,12 +2,19 @@ import { rate as exchangeRate } from "./finance.math.ts";
 import { Currency } from "./finance.type.ts";
 import { rate } from "./formatters/rate.ts";
 import { roubles } from "./formatters/rouble.ts";
+import { rupees } from "./formatters/rupiee.ts"
 import { usdt } from "./formatters/usdt.ts";
 
 export class Markdown {
-  static exchange(given: Currency, received: Currency): string {
+  static roubles2usdt(given: Currency, received: Currency): string {
     const result = exchangeRate(given, received);
     return `Поменял: ${roubles(given)} (${usdt(received)}). Курс: **${
+      rate(result)
+    }**`;
+  }
+  static usdt2lkr(given: Currency, received: Currency): string {
+    const result = exchangeRate(received, given);
+    return `Обмен USDT -> LKR: ${usdt(given)} (${rupees(received)}). Курс: **${
       rate(result)
     }**`;
   }
