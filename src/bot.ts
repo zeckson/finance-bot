367dc936c522fba0322b7fcc392fc62d0049a3f7
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2024-12-26 08:27:57 +0530

    add exchange command

--- src/bot.ts
+++ src/bot.ts
@@ -1,4 +1,6 @@
 import { Markup, Telegraf } from "telegraf";
+import { Markdown } from "./finance/finance.md.ts"
+import { escapeMarkdownV2 } from "./util/markdownv2.ts"
 
 const { BOT_TOKEN } = Deno.env.toObject();
 if (!BOT_TOKEN) throw new Error('"BOT_TOKEN" env var is required!');
@@ -10,6 +12,13 @@ const keyboard = Markup.inlineKeyboard([
 ]);
 
 bot.start((ctx) => ctx.reply("Hello"));
+bot.command(`exchange`, (ctx) => {
+  const [_, sent, received] = ctx.message.text.split(` `);
+  const reply = Markdown.roubles2usdt(Number(sent), Number(received))
+  return ctx.reply(escapeMarkdownV2(reply), {
+    parse_mode: `MarkdownV2`,
+  });
+});
 bot.help((ctx) => ctx.reply("Help message"));
 bot.on("message", (ctx) => ctx.copyMessage(ctx.message.chat.id, keyboard));
 bot.action("delete", (ctx) => ctx.deleteMessage());
---
+++ src/util/markdownv2.ts
@@ -0,0 +1,2 @@
+export const escapeMarkdownV2 = (text: string) =>
+  text.replace(/([_[\]()~`>#+\-=|{}.!])/g, "\\$1");
