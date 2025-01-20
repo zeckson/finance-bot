f2f02d509a127311b5e5a21c6474b9f5b6a3890d
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2025-01-20 22:05:57 +0530

    fix formatting

--- main.ts
+++ main.ts
@@ -1,16 +1,16 @@
-import "@std/dotenv/load";
+import '@std/dotenv/load'
 
-import { bot } from "./src/bot.ts";
+import { bot } from './src/bot.ts'
 
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
-  try {
-    await bot.launch(() => {
-      console.log(
-        `Telegram bot started: https://t.me/${bot.botInfo?.username}`,
-      );
-    });
-  } catch (err) {
-    console.error(err);
-  }
+	try {
+		await bot.launch(() => {
+			console.log(
+				`Telegram bot started: https://t.me/${bot.botInfo?.username}`,
+			)
+		})
+	} catch (err) {
+		console.error(err)
+	}
 }
